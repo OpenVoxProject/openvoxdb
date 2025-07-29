89e75fc71be8f5b46ec9a6b5a31aab4f4d7b5956
Josh Partlow <jpartlow@glatisant.org>
2025-07-29 14:15:34 -0700

    (gh-21) Abstract direct calls to sysv service
    There are a few spots in the suite where an attempt is made to 'service
    foo action' something directly, usually because we're testing status
    code or attempting to reload. EL9 and others no longer have service, so
    I've added a little service() helper to lib/helper.rb to pick systemctl
    or fallback to service or squawk.
    
    Where it appeared that puppet-resource would suffice, I switched to
    using that.
    
    However a little deeper in the rabbit hole lurks beaker-puppet which
    provides the with_puppet_running_on() method used throughout the suite,
    which calls a bounce_service method also in
    BeakerPuppet::Helpers::PuppetHelpers. And that method *also* attempts to
    use the sysv service executable...
    
    Right now I'm overriding this locally in helpers.rb to sidestep the
    issue, but we may want to bite the bullet, fork beaker-puppet and either
    depend on the forked repo directly or generate a different gem.
    
    Have not yet bitten the bullet. This is the same workaround I have in:
    openvoxproject/openvox-server@4758fec54d6a
    openvoxproject/openvox@2f85330b84d236f
    ...

--- acceptance/helper.rb
+++ acceptance/helper.rb
@@ -8,6 +8,7 @@ require 'set'
 require 'test/unit/assertions'
 require 'json'
 require 'inifile'
+require 'mkmf' # provides find_executable()
 
 module PuppetDBExtensions
   include Test::Unit::Assertions
@@ -242,7 +243,7 @@ module PuppetDBExtensions
 
   def start_puppetdb(host)
     step "Starting PuppetDB" do
-      on host, "service puppetdb start"
+      on(host, puppet_resource('service', 'puppetdb', 'ensure=running'))
       sleep_until_started(host)
     end
   end
@@ -724,11 +725,11 @@ module PuppetDBExtensions
   end
 
   def stop_puppetdb(host)
-    pids = puppetdb_pids(host)
-
-    on host, "service puppetdb stop"
-
-    sleep_until_stopped(host, pids)
+    step 'Stopping PuppetDB' do
+      pids = puppetdb_pids(host)
+      on(host, puppet_resource('service', 'puppetdb', 'ensure=stopped'))
+      sleep_until_stopped(host, pids)
+    end
   end
 
   def sleep_until_stopped(host, pids)
@@ -1105,6 +1106,59 @@ EOS
     databases[0]
   end
 end
+
+module Beaker
+  module DSL
+    module Helpers
+      module PuppetServerAcceptance
+        # A sad little abstraction around service/systemctl to handle
+        # os differences for edge cases not suited to puppet resource
+        # service calls.
+        def service(host, action, service_name, acceptable_exit_codes: [0])
+          if find_executable('systemctl')
+            command = Command.new("systemctl #{action} #{service_name}")
+          elsif find_executable('service')
+            command = Command.new("service #{service_name} #{action}")
+          else
+            raise "Neither systemctl nor service found on #{host.name}"
+          end
+
+          host.exec(command, acceptable_exit_codes: acceptable_exit_codes)
+        end
+
+        # Override beaker-puppet BeakerPuppet::Helpers::PuppetHelpers#bounce_service
+        # to allow for systems that only have systemctl now (el9+, etc.)
+        #
+        # Ostensibly we should fork beaker-puppet...I'm just not quite
+        # ready to touch that yet...
+        #
+        # Restarts the named puppet service
+        #
+        # @param [Host] host Host the service runs on
+        # @param [String] service Name of the service to restart
+        # @param [Fixnum] curl_retries Number of seconds to wait for the restart to complete before failing
+        # @param [Fixnum] port Port to check status at
+        #
+        # @return [Result] Result of last status check
+        # @!visibility private
+        def bounce_service(host, service, curl_retries = nil, port = nil)
+          curl_retries = 120 if curl_retries.nil?
+          port = options[:puppetserver_port] if port.nil?
+          result = service(host, :reload, service, acceptable_exit_codes: [0, 1, 3])
+          return result if result.exit_code == 0
+
+          host.exec puppet_resource('service', service, 'ensure=stopped')
+          host.exec puppet_resource('service', service, 'ensure=running')
+
+          curl_with_retries(" #{service} ", host, "https://localhost:#{port}", [35, 60], curl_retries)
+        end
+      end
+    end
+  end
+end
+
+# Register the DSL extension
+Beaker::DSL.register(Beaker::DSL::Helpers::PuppetServerAcceptance)
 
 # oh dear.
 Beaker::TestCase.send(:include, PuppetDBExtensions)
